d15413a9da4dc08aaa0093b8a6d4b8b64bcfe6a8
prototype-xk <ebaloche@gaming.tech>
2025-11-02 03:51:04 +0100

    Correction Bug + Opponent arrive
    Ajout Opponent.h
    Ajout Opponent.cpp
    Correction des Header

--- Edit_Perso_RPG/Edit_Perso_RPG/Class.h
+++ Edit_Perso_RPG/Edit_Perso_RPG/Class.h
@@ -11,44 +11,44 @@ protected:
     int AgilityBonus;
 };
 class Warrior : public Class {
-
+    Warrior();
 };
 class Mage : public Class {
-
+    Mage();
 };
 class Rogue : public Class {
-
+    Rogue();
 };
 class Cleric : public Class {
-
+    Cleric();
 };
 class Paladin : public Class {
-
+    Paladin();
 };
 class Ranger : public Class {
-
+    Ranger();
 };
 class Bard : public Class {
-
+    Bard();
 };
 class Necromancer : public Class {
-
+    Necromancer();
 };
 class Monk : public Class {
-
+    Monk();
 };
 class Sorcerer : public Class {
-
+    Sorcerer();
 };
 class Druid : public Class {
-
+    Druid();
 };
 class Barbarian : public Class {
-
+    Barbarian();
 };
 class Assassin : public Class {
-
+    Assassin();
 };
 class Alchemist : public Class {
-
+    Alchemist();
 };
--- Edit_Perso_RPG/Edit_Perso_RPG/Race.h
+++ Edit_Perso_RPG/Edit_Perso_RPG/Race.h
@@ -2,7 +2,7 @@
 class Race
 {
 public:
-
+	Race();
 protected:
 	int MaxHealth;
 	int Health;
@@ -15,61 +15,61 @@ protected:
 
 class Elf : public Race {
 public:
-
+	Elf();
 private:
 
 };
 class Dwarf : public Race {
 public:
-
+	Dwarf();
 private:
 
 };
 class Human : public Race {
 public:
-
+	Human();
 private:
 
 };
 class Orc : public Race {
 public:
-
+	Orc();
 private:
 
 };
 class Draconian : public Race {
 public:
-
+	Draconian();
 private:
 
 };
 class Undead : public Race {
 public:
-
+	Undead();
 private:
 
 };
 class Fairy : public Race {
 public:
-
+	Fairy();
 private:
 
 };
 class WereWolf : public Race {
 public:
-
+	WereWolf();
 private:
 
 };
 class HighElf : public Race {
 public:
-
+	HighElf();
 private:
 
 };
 class Serpentfolk : public Race {
 public:
-
+	Serpentfolk();
 private:
 
 };
