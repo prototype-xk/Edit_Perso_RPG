00e982ba247d768ed3c9e96aebad4d353059056d
prototype-xk <ebaloche@gaming.tech>
2025-11-04 18:21:51 +0100

    Start Add Weapon

--- Edit_Perso_RPG/Edit_Perso_RPG/Arme.h
+++ Edit_Perso_RPG/Edit_Perso_RPG/Arme.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include "Random.h"
-class Sword
+class Weapon
 {
 public:
 
@@ -9,4 +9,34 @@ protected:
 	std::string Name;	// Name of The Sword
 	int Damage;			// Number of Damage
 	int Type;			// Type of Damage ( Magic / Phyiscal / etc..* )
+};
+
+class Sword : public Weapon
+{
+public:
+
+protected:
+};
+
+class Bow : public Weapon
+{
+public:
+
+protected:
+};
+
+class Scepter : public Weapon
+{
+public:
+
+protected:
+	int Mana;
+};
+
+class Staff : public Weapon
+{
+public:
+
+protected:
+	int Mana;
 };
